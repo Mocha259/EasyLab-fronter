6b9d11f5ca0a46f348541a734fa6aaa7804781c0
Mocha259 <3378681490@qq.com>
2021-11-18 18:57:14 +0800

    提交一下，备份一下

--- src/components/Courses/CourseInfo/FileManage.vue
+++ src/components/Courses/CourseInfo/FileManage.vue
@@ -1,53 +1,88 @@
 <template>
     <div>
-         <el-upload
-         ref="upload"
+        <el-button @click="openDialog">上传文件</el-button>
+        <el-divider></el-divider>
+        <el-dialog title="上传" :visible.sync="dialogOfUpload" width="25%" style="text-align: center;">
+            <el-upload class="upload-demo" action="#" drag multiple :headers="headers" :auto-upload="false"
+                :file-list="fileList" :on-change="handleChange">
+                <i class="el-icon-upload"></i>
+                <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+                <!-- <div class="el-upload__tip" slot="tip">上传Excel格式文件</div> -->
+            </el-upload>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="dialogOfUpload = false">取 消</el-button>
+                <el-button type="primary" @click="confirmUpload">上 传</el-button>
+            </div>
+        </el-dialog>
+
+        <!-- <el-upload
+        ref="upload"
         class="upload-demo"
-        action="https://jsonplaceholder.typicode.com/posts/"
-       :on-success="handleAvatarSuccess"
-        :show-file-list="false">
+        :headers="headers"
+        action="#"
+        :show-file-list="false"
+        multiple 
+        :auto-upload="false"
+        :on-change="handleChange">
             <el-button size="big" type="primary" class="upload-button">上传文件</el-button>
             <div slot="tip" class="el-upload__tip">文件大小不能超过50M</div>
         </el-upload>
-        <br>
-        <div>
-       <el-table :data="fileList">
+        <br>-->
+        <div class="left" style="float: left; width: 20%">
+            <el-tree
+                :data="data"
+                node-key="id"
+                default-expand-all
+                @node-drag-start="handleDragStart"
+                @node-drag-enter="handleDragEnter"
+                @node-drag-leave="handleDragLeave"
+                @node-drag-over="handleDragOver"
+                @node-drag-end="handleDragEnd"
+                @node-drop="handleDrop"
+                draggable
+                :allow-drop="allowDrop"
+                :allow-drag="allowDrag">
+            </el-tree>
+        </div>
+        <div class="right" style="float: right; width: 70%"> 
+            <el-table :data="existedFileList">
 
-            <el-table-column
-            fixed
-            prop="name"
-            label="文件名称"
-            >
-            </el-table-column>
+                <el-table-column
+                fixed
+                prop="name"
+                label="文件名称"
+                >
+                </el-table-column>
 
-            <el-table-column
-            prop="size"
-            label="文件大小"
-            >
-            </el-table-column>
-             <!-- <template slot-scope="scope">
-                <span v-if="scope.row.attachSize / 1024 / 1024 < 1">{{(scope.row.attachSize / 1024).toFixed(2) + 'KB'}}</span>
-                <span v-else>{{(scope.row.attachSize / 1024 / 1024).toFixed(2) + 'MB'}}</span>
-            </template> -->
+                <el-table-column
+                prop="size"
+                label="文件大小"
+                >
+                </el-table-column>
+                <!-- <template slot-scope="scope">
+                    <span v-if="scope.row.attachSize / 1024 / 1024 < 1">{{(scope.row.attachSize / 1024).toFixed(2) + 'KB'}}</span>
+                    <span v-else>{{(scope.row.attachSize / 1024 / 1024).toFixed(2) + 'MB'}}</span>
+                </template> -->
 
 
 
-            <el-table-column
-            prop="date"
-            label="修改日期"
-            >
-            </el-table-column>
+                <el-table-column
+                prop="date"
+                label="修改日期"
+                >
+                </el-table-column>
 
-            <el-table-column
-            
-            label="操作"
-            >
-            <template >
-                <el-button  type="text" size="small" @click="handleExport">下载</el-button>
-                <el-button type="text" size="small">删除</el-button>
-            </template>
-            </el-table-column>
-        </el-table></div>
+                <el-table-column
+                
+                label="操作"
+                >
+                <template >
+                    <el-button  type="text" size="small" @click="handleExport">下载</el-button>
+                    <el-button type="text" size="small">删除</el-button>
+                </template>
+                </el-table-column>
+            </el-table>
+        </div>
        
         
     </div>
@@ -57,7 +92,11 @@
 export default {
     data() {
         return {
-            fileList: [{
+
+            dialogOfUpload: false,
+            headers: {},
+            fileList: [],		
+            existedFileList: [{
             name: '软件设计模式.mp4',
             size: '31M',
             date: '2021-11-16',
@@ -74,19 +113,104 @@ export default {
             size: '600k',
             date: '2021-11-13',
             }],
-            // fileList: [],
-            dataForm: {
-                id: 0,
-                noticeId: '',
-                attachName: '',
-                attachUrl: '',
-                attachSize: '',
-                },
-            dataList: [],
-
+            data: [{
+                id: 1,
+                label: '一级 1',
+                children: [{
+                    id: 4,
+                    label: '二级 1-1',
+                    children: [{
+                    id: 9,
+                    label: '三级 1-1-1'
+                    }, {
+                    id: 10,
+                    label: '三级 1-1-2'
+                    }]
+                }]
+                }, {
+                id: 2,
+                label: '一级 2',
+                children: [{
+                    id: 5,
+                    label: '二级 2-1'
+                }, {
+                    id: 6,
+                    label: '二级 2-2'
+                }]
+                }, {
+                id: 3,
+                label: '一级 3',
+                children: [{
+                    id: 7,
+                    label: '二级 3-1'
+                }, {
+                    id: 8,
+                    label: '二级 3-2',
+                    children: [{
+                    id: 11,
+                    label: '三级 3-2-1'
+                    }, {
+                    id: 12,
+                    label: '三级 3-2-2'
+                    }, {
+                    id: 13,
+                    label: '三级 3-2-3'
+                    }]
+                }]
+                }],
         }
     },
     methods: {
+         handleDragStart(node, ev) {
+            console.log('drag start', node);
+        },
+        handleDragEnter(draggingNode, dropNode, ev) {
+            console.log('tree drag enter: ', dropNode.label);
+        },
+        handleDragLeave(draggingNode, dropNode, ev) {
+            console.log('tree drag leave: ', dropNode.label);
+        },
+        handleDragOver(draggingNode, dropNode, ev) {
+            console.log('tree drag over: ', dropNode.label);
+        },
+        handleDragEnd(draggingNode, dropNode, dropType, ev) {
+            console.log('tree drag end: ', dropNode && dropNode.label, dropType);
+        },
+        handleDrop(draggingNode, dropNode, dropType, ev) {
+            console.log('tree drop: ', dropNode.label, dropType);
+        },
+        allowDrop(draggingNode, dropNode, type) {
+            if (dropNode.data.label === '二级 3-1') {
+            return type !== 'inner';
+            } else {
+            return true;
+            }
+        },
+        allowDrag(draggingNode) {
+            return draggingNode.data.label.indexOf('三级 3-2-2') === -1;
+        },
+        openDialog(){
+            this.dialogOfUpload = true
+        },
+        handleChange(file, fileList) { //文件数量改变
+			this.fileList = fileList;
+		},
+        confirmUpload() { //确认上传
+            var param = new FormData();
+            this.fileList.forEach(
+                (val, index) => {
+                    param.append("file", val.raw);
+                }
+            );
+
+            this.$http.post("/export/upload", param).then(responce => {});
+
+            this.$message({
+                message: "上传成功！",
+                duration: 1000
+            });
+        },
+
         goto() {
             let link = document.getElementById("linkUrl");
             let fileName = "测试下载默认Excel"
@@ -116,11 +240,23 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.el-table {
-    position: absolute;
-    left: 5%;
+.left {
     margin-top: 70px;
-    width: 88%;
+    float: left;
+    margin-left: 5%;
+    width: 30%;
+}
+.right {
+    margin-top: 70px;
+    float: right;
+    width: 100%;
+}
+.el-table {
+    // position: absolute;
+    // left: 5%;
+    // margin-top: 70px;
+    width: 100%;
+    float: right;
 }
 .el-table-column {
     width: 25%;
--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,7 +1,73 @@
 <template>
     <div>
-        <h3>
-            welcome
-        </h3>
+        <ul class="list">
+            <li
+                v-for="(item, index) in ulList"
+                :key="item.id"
+                :class="!index && play ? 'toUp' : ''"
+            >
+                {{ item.msg }}
+            </li>
+        </ul>
+
     </div>
 </template>
+
+<script>
+export default {
+  mounted() {
+    setInterval(this.startPlay, 2000)
+  },
+  data() {
+    return {
+      ulList: [
+        { msg: '这是第一条信息' },
+        { msg: '这是第二条信息' },
+        { msg: '这是第三条信息' },
+        { msg: '这是第四条信息' },
+        { msg: '这是第五条信息' },
+        { msg: '这是第六条信息' },
+        { msg: '这是第七条信息' },
+        { msg: '这是第八条信息' },
+        { msg: '这是第九条信息' },
+        { msg: '这是第十条信息' }
+      ],
+      play: false
+    }
+  },
+  methods: {
+    startPlay() {
+      let that = this
+      that.play = true //开始播放
+      setTimeout(() => {
+        that.ulList.push(that.ulList[0]) //将第一条数据塞到最后一个
+        that.ulList.shift() //删除第一条数据
+        that.play = false //暂停播放
+      }, 500)
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.toUp {
+  margin-top: -40px;
+  transition: all 0.5s;
+}
+
+.list {
+  list-style: none;
+  width: 200px;
+  text-align: center;
+  overflow: hidden;
+  height: 40px;
+  padding: 0;
+  margin: 0;
+}
+li {
+  text-align: left;
+  height: 40px;
+  line-height: 40px;
+}
+</style>
+
